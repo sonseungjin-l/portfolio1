1fa98770a358e599658b2a0be8bd84cc770d1ac0
sonseungjin-l <uncjumpoo@kakao.com>
2025-02-10 12:36:16 +0900

    Delete js directory

--- js/common.js
+++
@@ -1,8 +0,0 @@
-function search() {
-    const query = document.getElementById("search").value;
-    if (query) {
-        alert(`검색 실행: ${query}`);
-    } else {
-        alert("검색어를 입력하세요.");
-    }
-}
