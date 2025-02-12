b1743f0ebc0e78280319b43f78ed2e7eec3a87d3
sonseungjin-l <uncjumpoo@kakao.com>
2025-02-12 11:12:22 +0900

    Add files via upload

--- 세븐일레븐/js/common1.js
+++ 세븐일레븐/js/common1.js
@@ -23,3 +23,33 @@ $("#gnbList li").hover(function(){
 //     else
 //     $("#header").css({backgroundColor: 'transparent'})
 // })
+
+//행사 상품 목록
+// const products = {
+//     "1+1": [
+//         { name: "2080)슈퍼클린칫솔", price: "2,000원", <img src='' > "" },
+//         { name: "케라시스)볼륨클리닉샴푸 750ML(F)", price: "11,000원", img: "shampoo1.png" },
+//         { name: "케라시스)샴푸데미지250g", price: "5,500원", img: "shampoo2.png" },
+//         { name: "케라시스)볼륨린스750ML(F)", price: "11,000원", img: "rinse.png" }
+//     ],
+//     "2+1": [],
+//     "new": []
+// };
+
+// function showProducts(category) {
+//     const productList = document.getElementById('productList');
+//     productList.innerHTML = '';
+    
+//     products[category].forEach(product => {
+//         const div = document.createElement('div');
+//         div.className = 'product';
+//         div.innerHTML = `
+//             <img src="${product.img}" alt="${product.name}">
+//             <p>${product.name}</p>
+//             <p class="price">${product.price}</p>
+//         `;
+//         productList.appendChild(div);
+//     });
+// }
+
+// showProducts('1+1'); // 초기 표시
